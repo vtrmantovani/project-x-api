8f15cd69bb66a885764fd7907fb5e01039ca792d
Vitor Mantovani <vtrmantovani@gmail.com>
2018-09-16 10:16:42 -0300

    Create api for get website urls

--- pxa/views/website.py
+++ pxa/views/website.py
@@ -1,7 +1,12 @@
+from elasticsearch.exceptions import ElasticsearchException
 from flask import Blueprint, jsonify, request
+from sqlalchemy.exc import SQLAlchemyError
+from werkzeug.exceptions import NotFound
 
-from pxa import db
+from pxa import db, logger
+from pxa.models.no_sql.website import WebsiteNoSql
 from pxa.models.website import Website
+from pxa.utils.api_exceptions import BadRequestGeneric
 from pxa.utils.decorators import (requires_api_key, requires_fields_validation,
                                   requires_json)
 
@@ -17,7 +22,30 @@ def create():
     website = Website()
     website.url = r['url']
     website.status = Website.Status.NEW
-    db.session.add(website)
-    db.session.commit()
-
+    try:
+        db.session.add(website)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        logger.error("SQLAlchemyError error in create website: {0}".format(e))
+        raise BadRequestGeneric(description="Some problems in bd")
     return jsonify(), 201
+
+
+@requires_api_key
+@bp_website.route('/website/<p_website_id>', methods=['GET'])
+def get(p_website_id):
+    try:
+        website_db = WebsiteNoSql()
+        website = website_db.get(p_website_id)
+        if not website:
+            raise NotFound("Website id not found")
+
+        urls = website['_source']
+        if not urls:
+            logger.error("Source not found on website_id={}".format(p_website_id))  # noqa
+            raise BadRequestGeneric(description="Source not found")
+    except ElasticsearchException as e:
+        logger.error("ElasticsearchException error in get website_id:{0}. Error:{1}".format(p_website_id, e))  # noqa
+        raise BadRequestGeneric(description="Some problems in bd")
+
+    return jsonify(website['_source'])
--- tests/views/test_website.py
+++ tests/views/test_website.py
@@ -1,5 +1,10 @@
 import json
 
+import mock
+from elasticsearch.exceptions import ElasticsearchException
+from sqlalchemy.exc import SQLAlchemyError
+
+from pxa.models.no_sql.website import WebsiteNoSql
 from pxa.models.website import Website
 from tests.base import BaseTestCase
 
@@ -22,3 +27,63 @@ class TestViewWebsite(BaseTestCase):
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Website.query.count(), 1)
+
+    @mock.patch('pxa.db.session.add')
+    def test_create_website_with_sqlalchemy_error(self, mock_db):
+        mock_db.side_effect = mock.Mock(side_effect=SQLAlchemyError())
+        params = {
+            "url": "http://vtrmantovani.com.br",
+        }
+        response = self.client.post("/api/website", headers={
+            'Authorization': self.api_key,
+            'Content-Type': 'application/json'
+        }, data=json.dumps(params))
+        r = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(r['error']['message'], "Some problems in bd")
+
+    def test_get_website(self):
+        webiste_nosql = WebsiteNoSql()
+        body = {
+            'urls': ['http://ibm.com.br']
+        }
+        webiste_nosql.create(1, body)
+        response = self.client.get(
+            '/api/website/1',
+            headers={'Authorization': 'PXaLogin apikey="xpo"'}
+        )
+        r = json.loads(response.data.decode('utf-8'))
+        self.assertEquals(r['urls'][0], 'http://ibm.com.br')
+        self.assertEqual(response.status_code, 200)
+        webiste_nosql.delete(1)
+
+    def test_get_website_notfound(self):
+        response = self.client.get(
+            '/api/website/562',
+            headers={'Authorization': 'PXaLogin apikey="xpo"'}
+        )
+        r = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(r['error']['status'], 404)
+        self.assertEqual(r['error']['message'], 'Website id not found')
+
+    @mock.patch('pxa.models.no_sql.website.WebsiteNoSql.get')
+    def test_get_website_source_not_found(self, mock_get):
+        mock_get.return_value = {'_source': ''}
+        response = self.client.get(
+            '/api/website/1',
+            headers={'Authorization': 'PXaLogin apikey="xpo"'}
+        )
+        r = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(r['error']['message'], "Source not found")
+
+    @mock.patch('pxa.models.no_sql.website.WebsiteNoSql.get')
+    def test_get_website_with_elasticsearch_exception(self, mock_get):
+        mock_get.side_effect = mock.Mock(side_effect=ElasticsearchException())
+        response = self.client.get(
+            '/api/website/1',
+            headers={'Authorization': 'PXaLogin apikey="xpo"'}
+        )
+        r = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(r['error']['message'], "Some problems in bd")
